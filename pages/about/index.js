6c8b72d0b96324c1521bf17ba009ecf9da03f1ae
yacine <lentente1958@gmail.com>
2023-05-18 11:33:08 +0100

    define pages

---
+++ pages/about/index.js
@@ -0,0 +1,5 @@
+export default function about(){
+    return <>
+    <h1>Hello from About page</h1>
+    </>
+}
---
+++ pages/profil/index.js
@@ -0,0 +1,5 @@
+export default function profil(){
+    return <>
+    <h1>Hello from Profil page</h1>
+    </>
+}
